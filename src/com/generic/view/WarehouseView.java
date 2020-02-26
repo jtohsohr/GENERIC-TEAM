249da0d0d9193f5f95131958d8b88de8d25db088
Bossmanswift <olaoluwaseyi81@gmail.com>
2020-02-26 00:00:07 -0600

    more features added

--- src/com/generic/view/WarehouseView.java
+++ src/com/generic/view/WarehouseView.java
@@ -15,7 +15,11 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -24,14 +28,11 @@ public class WarehouseView extends Application{
 
 	// private static WarehouseView warehouseView;
 	private Stage primaryStage;
-	private MenuBar menuBar;
-	private Menu file;
-	private MenuItem menuItemImport;
-	private MenuItem menuItemExport;
-	private MenuItem menuItemExit;
+	
 	
 	private TableView<Warehouse> warehouseTable;
 	private Button toogleFreightBtn;
+	private TextField wIDTextField, wNameTextField, wFreightStatus;
 		
 	
 	public static void main(String[] args) {
@@ -51,6 +52,7 @@ public class WarehouseView extends Application{
 	
 	
 	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
@@ -62,7 +64,7 @@ public class WarehouseView extends Application{
 		
 		// Warehouse Name Column
 		TableColumn<Warehouse, String> wNameColumn = new TableColumn<>("Name");
-		wNameColumn.setMinWidth(200);
+		wNameColumn.setMinWidth(400);
 		wNameColumn.setCellValueFactory(new PropertyValueFactory<Warehouse, String>("WarehouseName"));
 		
 		// Warehouse ID Column
@@ -72,12 +74,12 @@ public class WarehouseView extends Application{
 		
 		// Warehouse Shipments Size Column
 		TableColumn<Warehouse, Integer> wShipmentsSize = new TableColumn<>("Shipments Avaliable");
-		wShipmentsSize.setMinWidth(120);
+		wShipmentsSize.setMinWidth(200);
 		wShipmentsSize.setCellValueFactory(new PropertyValueFactory<Warehouse, Integer> ("ShipmentsSize"));
 		
 		// Warehouse Freight Status Column
 		TableColumn<Warehouse, String> wFreightStatusColumn = new TableColumn<Warehouse, String>("Freight Status");
-		wFreightStatusColumn.setMinWidth(120);
+		wFreightStatusColumn.setMinWidth(200);
 		wFreightStatusColumn.setCellValueFactory(new PropertyValueFactory<Warehouse, String> ("FreightStatus"));
 		
 		// Add Columns to TableView
@@ -87,14 +89,66 @@ public class WarehouseView extends Application{
 		Label tableHeadingLabel = new Label("Warehouse Inventory");
 		tableHeadingLabel.setFont(new Font("Arial", 20));
 		
-		// Add table to parent pane
-		VBox mainPane = new VBox(10);
-		mainPane.setPadding(new Insets(10));
-		mainPane.getChildren().addAll(tableHeadingLabel, warehouseTable);
-				
+		// Add table to center pane
+		VBox centerPane = new VBox(10);
+		centerPane.setPadding(new Insets(10));
+		centerPane.getChildren().addAll(tableHeadingLabel, warehouseTable);
+		
+		
+		// Top pane contents
+		Menu fileMenu = new Menu("File");
+		Menu importMenuItem = new Menu("Import");
+		Menu exportMenuItem = new Menu("Export");
+
+		
+		fileMenu.getItems().addAll(importMenuItem, exportMenuItem);
+		
+		MenuBar menuBar = new MenuBar(fileMenu);
+		
+		
+		
+		HBox topPane = new HBox(10);
+		topPane.getChildren().addAll(menuBar);
+		
+		
+		// 	
+		wNameTextField = new TextField();
+		wNameTextField.setPromptText("Name");
+		wNameTextField.setMinWidth(200);
+		
+		wIDTextField = new TextField();
+		wIDTextField.setPromptText("ID");
+		wIDTextField.setMinWidth(100);
+		
+		wFreightStatus = new TextField();
+		wFreightStatus.setPromptText("E(Enable Freight Receipt)/D(Disable Freight Receipt)");
+		wFreightStatus.setMinWidth(400);
+		
+		Button addBtn = new Button("Add");
+		addBtn.setMinWidth(60);
+		
+		Button deleteBtn = new Button("Delete");
+		deleteBtn.setMinWidth(60);
+		
+		HBox bottomPane = new HBox();
+		bottomPane.setPadding(new Insets(10));
+		bottomPane.setSpacing(8);
+		bottomPane.getChildren().addAll(wIDTextField, wNameTextField, wFreightStatus, addBtn, deleteBtn);
+		
+		
+		
+		
+		
+		
+		
+		
+		BorderPane borderPane = new BorderPane();
+		borderPane.setTop(topPane);
+		borderPane.setCenter(centerPane);
+		borderPane.setBottom(bottomPane);
 		
 		// Set the Scene
-		Scene scene = new Scene(mainPane);
+		Scene scene = new Scene(borderPane);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("G.T TRACKER");
 		primaryStage.show();
